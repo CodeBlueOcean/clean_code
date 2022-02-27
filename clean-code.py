c441c6dde323affa0136574bad4115cb0a3421a8
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-26 20:14:36 -0600

    Add files via upload

---
+++ clean-code.py
@@ -0,0 +1,18 @@
+# clean code
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    elif num % 2 != 0:
+        return False
+
+print(is_even(51))
+
+# clean code
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else: 
+        return False
+
+print(is_even(51))
+
